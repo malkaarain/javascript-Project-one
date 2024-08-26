a7ec6685431376ecf95d9741bb429983661f09ab
Malka arain <malkaarain92@gmail.com>
2024-08-27 01:31:59 +0500

    Add files via upload

---
+++ Chapter-Five/script.js
@@ -0,0 +1,160 @@
+//Question no 1
+// var num1 = 3
+// var num2 = 5
+// var num3 = (num1 + num2)
+
+// document.write("Sum of " + num1 + "and " + num2 + "is" + num3 )
+
+//Question no 2
+
+// var num1 = 3
+// var num2 = 5
+// var num3 = (num1 - num2)
+
+// document.write("Sum of " + num1 + "and " + num2 + "is" + num3 )
+
+
+//Question no 3
+// (A).
+// var name
+
+// (B).
+// var price
+// document.write("Value after variable declaration is:" + price)
+
+// (C).
+// var price = 5
+// document.write(price)
+
+// (D).
+// var price = 5
+// document.write("Initialize the variable:" + price)
+
+// (E).
+// var price = 5
+// document.write(++price)
+
+// (F).
+// var price = 5
+// document.write("Value after increment is:" + ++price)
+
+// (G).
+// var price1 = 5
+// var price2 = 7
+// var price3 = price1 + price2
+// document.write(++price3)
+
+// (H).
+// var price1 = 5
+// var price2 = 7
+// var price3 = price1 + price2
+// document.write("Value after addition is:" + ++price3)
+
+// (I).
+// var price1 = 5
+// var price2 = 7
+// var price3 = price1 + price2
+// document.write(price3--)
+
+// // (J).
+// var price1 = 5
+// var price2 = 7
+// var price3 = price1 + price2
+// document.write("Value after decrement is:" + price3--)
+
+// (K).
+// var price1 = 5
+// var price2 = 7
+// var price3 = price1 / price2
+// document.write(price3)
+
+// (L).
+// var price1 = 5
+// var price2 = 7
+// var price3 = price1 / price2
+// document.write("The remainder is :" + price3)
+
+
+
+
+
+//Question no 4
+// var ticketPrice = 600;
+// var totalCost = ticketPrice * 5;
+
+// document.write("Total cost of buying 5 movie tickets is: " + totalCost + " PKR");
+
+
+
+//Question no 5
+
+// var num9 = 4
+
+// document.write(`${num9} * 1 = ${num9 * 1}`)
+// document.write(`${num9} * 2 = ${num9 * 2}`)
+// document.write(`${num9} * 3 = ${num9 * 3}`)
+// document.write(`${num9} * 4 = ${num9 * 4}`)
+// document.write(`${num9} * 5 = ${num9 * 5}`)
+// document.write(`${num9} * 6 = ${num9 * 6}`)
+// document.write(`${num9} * 7 = ${num9 * 7}`)
+// document.write(`${num9} * 8 = ${num9 * 8}`)
+// document.write(`${num9} * 9 = ${num9 * 9}`)
+// document.write(`${num9} * 10 = ${num9 * 10}`)
+
+
+
+
+
+
+
+
+//Question no 7
+
+ // a. 
+//  let priceItem1 = 650;
+
+//  // b. 
+//  let priceItem2 = 100;
+
+//  // c. 
+//  let quantityItem1 = 3;
+
+//  // d. 
+//  let quantityItem2 = 7;
+
+//  // e. 
+//  let shippingCharges = 100;
+
+
+//  let totalCost = (priceItem1 * quantityItem1) + (priceItem2 * quantityItem2) + shippingCharges;
+
+
+//  document.write("<h2>Shopping Cart Checkout</h2>");
+//  document.write("Price of item 1: " + priceItem1 + "<br>");
+//  document.write("Ordered quantity of item 1is " + quantityItem1 + "<br>");
+//  document.write("Price of item 2: " + priceItem2 + " PKR<br>");
+//  document.write("Ordered quantity of item 2is " + quantityItem2 + "<br>");
+//  document.write("Shipping charges: " + shippingCharges + " PKR<br>");
+//  document.write("<br><strong>Total cost: " + totalCost + " PKR</strong>");
+
+
+
+
+
+//Question no 10
+// var add = 5
+// var add1 = 2
+// var mul = 15
+// var divi = 5
+
+// document.write (add + add1 * mul / divi)
+
+
+
+//Question no 11
+// var current = 2016
+// document.write ("Current year:" + current)
+// var birth = 1992
+// document.write ("Birth year:" + birth )
+
+// document.write ("Your age is:" + current - birth )
---
+++ Chapter-Four/script.js
@@ -0,0 +1,40 @@
+// Question no 1
+// var  x,y,z
+
+// Question no 2
+//  legal
+// firstName
+// total_amount
+// user2
+
+// illegal
+// 2ndNumber 
+// user-age
+// & 
+// $salary
+
+
+// Question no 2
+//  (B)
+// var number1 = (",")
+// var number2 = ("numbers")
+// var number3 = ("$")
+// var number4 = ("_")
+// document.write ("Variable names can only contain"+ number1 + number2 + number3 +"and"+ number4 +".For example $my_1stVariable")
+
+
+//  (C)
+// var number1 = ("Letter")
+// var number2 = ("$")
+// var number3 = ("_")
+// document.write ("Variables must begin with a"+ number1 + number2 +"or" + number3 +".For example $name, _name or name")
+
+//  (D)
+// var number1 = ("sensitive")
+// document.write ("Variable names are case" + number1)
+
+//  (E)
+// var number1 = ("keyword")
+// document.write ("Variable names are case" + number1)
+
+
